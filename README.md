2227f84f805fbbea494edd7f5a00484096d7c46a
r <radu@raicea.com>
2017-10-28 12:16:18 -0400

    add forecast for next hour and day

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Weather Bot
+# Charlie
 Slack bot for getting weather updates.
 
 <a href="https://darksky.net/"><img src="https://darksky.net/dev/img/attribution/poweredby-oneline.png" width="200"></a>
--- bot.py
+++ bot.py
@@ -14,19 +14,53 @@ bot_id = config.SLACK_BOT_ID
 
 
 def validates_message(event):
-    if event['type'] == 'message' and event['user'] != bot_id and re.match(r'.*weather.*', event['text'].lower()):
+    if event['type'] == 'message' and event['user'] != bot_id and re.match(r'.*charlie.*weather.*', event['text'].lower()):
         return True
+    elif event['type'] == 'message' and event['user'] != bot_id and re.match(r'.*charlie bit me.*', event['text'].lower()):
+        charlie_bit_me(event['channel'])
 
 
-def handle_message(message, user, channel):
-    response = requests.get(url)
-    forecast = response.json()
-    message = '*Current Weather*\n%s\nFeels like %s' % (forecast['currently']['summary'], forecast['currently']['apparentTemperature'])
+def handle_message(message, channel):
+
+    message = message.lower()
+
+    if re.match(r'.*(today|day).*', message):
+        message = today_weather()
+    elif re.match(r'.*(hour).*', message):
+        message = next_hour_weather()
+    else:
+        message = current_weather()
+        
     send_message(message=message, channel=channel)
 
 
+def current_weather():
+    new_url = url + '&exclude=%s,%s,%s,%s,%s' % ('minutely', 'hourly', 'daily', 'alerts', 'flags')
+    response = requests.get(new_url)
+    forecast = response.json()
+    return '[{"color": "#36a64f", "title": "Current Weather", "text": "%s\nFeels like %s"}]' % (forecast['currently']['summary'], forecast['currently']['apparentTemperature'])
+
+
+def next_hour_weather():
+    new_url = url + '&exclude=%s,%s,%s,%s,%s' % ('currently', 'minutely', 'daily', 'alerts', 'flags')
+    response = requests.get(new_url)
+    forecast = response.json()
+    return '[{"color": "#36a64f", "title": "Weather in the Next Hour", "text": "%s\nFeels like %s"}]' % (forecast['hourly']['data'][0]['summary'], forecast['hourly']['data'][0]['apparentTemperature'])
+
+
+def today_weather():
+    new_url = url + '&exclude=%s,%s,%s,%s,%s' % ('currently', 'minutely', 'hourly', 'alerts', 'flags')
+    response = requests.get(new_url)
+    forecast = response.json()
+    return '[{"color": "#36a64f", "title": "Today\'s Weather", "text": "%s\nFeels like %s (High) and %s (Low)"}]' % (forecast['daily']['data'][0]['summary'], forecast['daily']['data'][0]['apparentTemperatureMax'], forecast['daily']['data'][0]['apparentTemperatureMin'])
+
+
+def charlie_bit_me(channel):
+    bot.api_call('chat.postMessage', channel=channel, text=':cry:', as_user=True)
+
+
 def send_message(message, channel):
-    bot.api_call('chat.postMessage', channel=channel, text=message, as_user=True)
+    bot.api_call('chat.postMessage', channel=channel, attachments=message, as_user=True)
 
 
 def run():
@@ -35,7 +69,7 @@ def run():
         while True:
             for event in bot.rtm_read():
                 if validates_message(event):
-                    handle_message(message=event.get('text'), user=event.get('user'), channel=event.get('channel'))
+                    handle_message(message=event.get('text'), channel=event.get('channel'))
             time.sleep(1)
     else:
         print('Connection Failed')
